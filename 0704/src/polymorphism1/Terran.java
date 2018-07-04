accc4dc1e7013a4dfe8dc3bc82c00870fa3d01ee
15522 <rnslss@naver.com>
2018-07-04 18:00:42 +0900

    commit

---
+++ 0704/src/polymorphism1/Terran.java
@@ -0,0 +1,12 @@
+package polymorphism1;
+
+public class Terran extends Starcraft {
+
+		// 공격하는 메소드
+		public void attack() {
+		System.out.println("공격하는 테란 ");
+		
+		}
+	}
+
+
---
+++ 0704/src/polymorphism1/protoss.java
@@ -0,0 +1,9 @@
+package polymorphism1;
+
+public class protoss extends Starcraft {
+	//공격하는 메소드
+	public void attack() {
+		System.out.println("프로토스의 공격");
+	}
+
+}
